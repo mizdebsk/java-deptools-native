9fd669b214efa4eebea51842c08c9db2ded88632
Marián Konček <marian.koncek@mailbox.org>
2023-11-22 10:57:56 +0100

    Add more RpmDependency tests

--- src/test/java/io/kojan/javadeptools/rpm/RpmDependencyTest.java
+++ src/test/java/io/kojan/javadeptools/rpm/RpmDependencyTest.java
@@ -19,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
 
@@ -65,5 +66,82 @@ public class RpmDependencyTest {
         assertEquals("", dep4.getSense());
         assertEquals("", dep4.getVersion().toString());
         assertFalse(dep4.isRich());
+
+        Iterator<RpmDependency> provIt = info.getProvides().iterator();
+        RpmDependency provs1 = provIt.next();
+        assertEquals("rpmtags = 4242424242:1-1", provs1.toString());
+        assertEquals("rpmtags", provs1.getName());
+        assertEquals("=", provs1.getSense());
+        assertEquals("4242424242:1-1", provs1.getVersion().toString());
+        assertFalse(provs1.isRich());
+
+        RpmDependency provs2 = provIt.next();
+        assertEquals("which <= 10", provs2.toString());
+        assertEquals("which", provs2.getName());
+        assertEquals("<=", provs2.getSense());
+        assertEquals("10", provs2.getVersion().toString());
+        assertFalse(provs2.isRich());
+
+        assertFalse(provIt.hasNext());
+
+        Iterator<RpmDependency> conflIt = info.getConflicts().iterator();
+        RpmDependency confl1 = conflIt.next();
+        assertEquals("make = 2", confl1.toString());
+        assertEquals("make", confl1.getName());
+        assertEquals("=", confl1.getSense());
+        assertEquals("2", confl1.getVersion().toString());
+        assertFalse(confl1.isRich());
+
+        assertFalse(conflIt.hasNext());
+
+        Iterator<RpmDependency> obsIt = info.getObsoletes().iterator();
+        RpmDependency obs1 = obsIt.next();
+        assertEquals("rpmtags < 11", obs1.toString());
+        assertEquals("rpmtags", obs1.getName());
+        assertEquals("<", obs1.getSense());
+        assertEquals("11", obs1.getVersion().toString());
+        assertFalse(obs1.isRich());
+
+        assertFalse(obsIt.hasNext());
+
+        Iterator<RpmDependency> sugIt = info.getSuggests().iterator();
+        RpmDependency sug1 = sugIt.next();
+        assertEquals("java-latest-openjdk > 8", sug1.toString());
+        assertEquals("java-latest-openjdk", sug1.getName());
+        assertEquals(">", sug1.getSense());
+        assertEquals("8", sug1.getVersion().toString());
+        assertFalse(sug1.isRich());
+
+        assertFalse(sugIt.hasNext());
+
+        Iterator<RpmDependency> supIt = info.getSupplements().iterator();
+        RpmDependency sup1 = supIt.next();
+        assertEquals("tmt = 1", sup1.toString());
+        assertEquals("tmt", sup1.getName());
+        assertEquals("=", sup1.getSense());
+        assertEquals("1", sup1.getVersion().toString());
+        assertFalse(sup1.isRich());
+
+        assertFalse(supIt.hasNext());
+
+        Iterator<RpmDependency> enhIt = info.getEnhances().iterator();
+        RpmDependency enh1 = enhIt.next();
+        assertEquals("ant < 5", enh1.toString());
+        assertEquals("ant", enh1.getName());
+        assertEquals("<", enh1.getSense());
+        assertEquals("5", enh1.getVersion().toString());
+        assertFalse(enh1.isRich());
+
+        assertFalse(enhIt.hasNext());
+
+        Iterator<RpmDependency> orderIt = info.getOrderWithRequires().iterator();
+        RpmDependency order1 = orderIt.next();
+        assertEquals("maven >= 3", order1.toString());
+        assertEquals("maven", order1.getName());
+        assertEquals(">=", order1.getSense());
+        assertEquals("3", order1.getVersion().toString());
+        assertFalse(order1.isRich());
+
+        assertFalse(orderIt.hasNext());
     }
 }
